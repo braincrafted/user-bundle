fc5ea68087d4cd6e64f1c69dfd4dcfc5b3179461
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-05-27 15:41:59 +0200

    Delete account and improved design of profile

---
+++ Controller/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Bc\Bundle\UserBundle\Controller;
+
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\ProfileController as BaseProfileController;
+use FOS\UserBundle\Model\UserInterface;
+
+class ProfileController extends BaseProfileController
+{
+    /**
+     * Delete action.
+     *
+     */
+    public function deleteAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $this->container->get('bc_user.user_manager')->deleteUser($user);
+
+        return new RedirectResponse(
+            $this->container->get('router')->generate(
+                'bc_user_delete_success',
+                array('username' => $user->getUsername())
+            )
+        );
+    }
+
+    public function deleteSuccessAction($username)
+    {
+        return $this->container->get('templating')->renderResponse(
+            'BcUserBundle:Profile:deleteSuccess.html.'.$this->container->getParameter('fos_user.template.engine'),
+            array('username' => $username)
+        );
+    }
+}
